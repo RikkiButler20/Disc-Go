aa73421c8825b1f5f52adbf26eb03b9963687b86
Richarda <39577672+RikkiButler20@users.noreply.github.com>
2020-05-07 11:29:02 -0500

    Creating UI project

---
+++ Disc-Go/Disc-Go/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Disc-Go
+//
+//  Created by Kayla Butler on 5/7/20.
+//  Copyright © 2020 Kayla Butler. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Disc-Go/Disc-Go/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Disc-Go
+//
+//  Created by Kayla Butler on 5/7/20.
+//  Copyright © 2020 Kayla Butler. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
