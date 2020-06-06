782c1b3e668e30d1dbd8c3f8481edd93c5d60c3b
Richarda <39577672+RikkiButler20@users.noreply.github.com>
2020-06-06 13:44:43 -0500

    FP Connects made the connections from UI to forgot password view controller

--- Disc-Go/Disc-Go/ForgotPassViewController.swift
+++ Disc-Go/Disc-Go/ForgotPassViewController.swift
@@ -10,13 +10,20 @@ import UIKit
 
 class ForgotPassViewController: UIViewController {
 
-    @IBOutlet weak var username: UITextField!
     @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var securityQuestion: UITextField!
+    @IBOutlet weak var securityAnswer: UITextField!
     
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func userSecurityQuestion(_ sender: Any) {
+    }
+    @IBAction func resetPassword(_ sender: Any) {
+    }
+    
 }
