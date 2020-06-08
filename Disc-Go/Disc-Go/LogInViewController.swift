d8016615f570bc5a1a6b64be9ccf1f497d5b56a0
Richarda <39577672+RikkiButler20@users.noreply.github.com>
2020-06-07 22:39:57 -0500

    Working on the log in page working

--- Disc-Go/Disc-Go/LogInViewController.swift
+++ Disc-Go/Disc-Go/LogInViewController.swift
@@ -27,16 +27,48 @@ class LogInViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-    @IBAction func logIn(_ sender: Any) {
+    @IBAction func logIn(_ sender: Any) { //TODO: Fix when backend comes in
         //if both fields aren't filled out
-            //return
+        let usernameText = username.text ?? ""
+        let passwordText = password.text ?? ""
+        
+        if usernameText.isEmpty || passwordText.isEmpty {
+            return
+        }
         
         //if username exists
+        if userDB[usernameText] != nil {
             //check if password matches that password
+            
+            guard let user = userDB[usernameText] else {
+                print("couldn't get name from dictionary")
+                return
+            }
+            
+            if passwordText == user[3] { //getting the password from the array
                 //curUser = the array of the user we got from DB & include the username
-                //segue to account page
+                var curUser = user
+                curUser.append(usernameText)
+                print("Logging In")
+                
+                //segue to account page TODO: Do the segue
+            } else {
+                incorrectPasswordAlert()
+            }
+        }
             //pop up of password doesn't match
+            
         //pop up of username doesn't exist
+    }
+    
+    func incorrectPasswordAlert() {
+        let alert = UIAlertController(title: "Alert", message: "Password is incorrect", preferredStyle: .alert)
+
+        alert.addAction(UIAlertAction(title: "Close", style: .default, handler: { (_) in
+            print("You closed the incorrect password alert")
+        }))
+
+        self.present(alert, animated: true, completion: nil)
     }
     
 }
