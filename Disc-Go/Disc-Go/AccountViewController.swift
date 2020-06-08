94eeaf25dfb01df446436b99940d34f9ead09023
Richarda <39577672+RikkiButler20@users.noreply.github.com>
2020-06-07 23:03:33 -0500

    Create account view

---
+++ Disc-Go/Disc-Go/AccountViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AccountViewController.swift
+//  Disc-Go
+//
+//  Created by Kayla Butler on 6/7/20.
+//  Copyright © 2020 Kayla Butler. All rights reserved.
+//
+import UIKit
+
+class AccountViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+}
+
