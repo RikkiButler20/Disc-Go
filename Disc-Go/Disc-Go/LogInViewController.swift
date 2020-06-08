c7325a40cb55f3adaf0e11d72dca2fcbc38af9a0
Richarda <39577672+RikkiButler20@users.noreply.github.com>
2020-06-07 19:49:51 -0500

    Fake data Creating fake user data

--- Disc-Go/Disc-Go/LogInViewController.swift
+++ Disc-Go/Disc-Go/LogInViewController.swift
@@ -13,6 +13,14 @@ class LogInViewController: UIViewController {
     @IBOutlet weak var username: UITextField!
     @IBOutlet weak var password: UITextField!
     
+    //creating fake user DB
+    //[first name, last name, email, password, security question, security answer]
+    var userDB = [
+        "shanetoohotforyou" : ["Shane", "May", "shanetoohotforyou@gmail.com", "Password2013", "Favorite color", "Blue"],
+        "KayRB1" : ["Kayla", "Butler", "kayla.butler8@icloud.com", "Pa55w0rd", "Fav show", "Supernatural"],
+        "ImWraith" : ["Wraith", "Surname", "iamthevoid@hotmail.com", "VoidWalker1", "Favorite gun", "Wingman"]
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
