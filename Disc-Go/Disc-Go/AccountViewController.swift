82178a7d1948fe2b8e69e60a809a0fdcdc609741
Richarda <39577672+RikkiButler20@users.noreply.github.com>
2020-06-15 21:51:54 -0500

    Got the tab working

--- Disc-Go/Disc-Go/AccountViewController.swift
+++ Disc-Go/Disc-Go/AccountViewController.swift
@@ -9,9 +9,10 @@ import UIKit
 
 class AccountViewController: UIViewController {
     
+    @IBOutlet weak var toMap: UITabBarItem!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view
     }
     
 
