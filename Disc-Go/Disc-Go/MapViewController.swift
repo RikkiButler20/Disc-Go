e15248798d1f68e470ae02993764a028de4779fb
Richarda <39577672+RikkiButler20@users.noreply.github.com>
2020-07-01 22:18:03 -0500

    Adding location manager

--- Disc-Go/Disc-Go/MapViewController.swift
+++ Disc-Go/Disc-Go/MapViewController.swift
@@ -2,17 +2,40 @@
 //  MapViewController.swift
 //  Disc-Go
 //
-//  Created by Kayla Butler on 6/15/20.
+//  Created by Kayla Butler on 7/1/20.
 //  Copyright © 2020 Kayla Butler. All rights reserved.
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class MapViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    fileprivate let locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
+        locationManager.requestWhenInUseAuthorization();
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.distanceFilter = kCLDistanceFilterNone
+        locationManager.startUpdatingLocation()
+
+        mapView.showsUserLocation = true
     }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
